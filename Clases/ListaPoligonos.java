64144ea355e45c0edb2c5d528bfc35d523788aeb
kevv87 <7m6JHC84iMsw>
2018-09-07 17:32:03 -0600

    Clase lista de poligonos anadida

---
+++ Clases/ListaPoligonos.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import java.awt.Polygon;
+
+/**
+ *
+ * @author kevv87
+ */
+public class ListaPoligonos extends ListaSimple{
+    /**
+     * Sobreescribiendo el metodo para que el tipo del valor que devuelva sea una Linea.
+     * @param pos Posicion en el array, debe estar entre 0 y n-1
+     * @return Linea en la posicion pos
+     * @throws java.lang.Exception
+     */
+    @Override
+    public Polygon getValor(int pos) throws Exception{
+        Polygon result = (Polygon)super.getValor(pos);
+        return result;
+    }
+}
