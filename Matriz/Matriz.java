b2adaa1a229e1db7698f5b15b09eb16d1c97f0fc
fabian152195 <fabian152195@gmail.com>
2018-09-11 18:06:56 -0600

    Matirz: funcion agrega fila

--- Matriz/Matriz.java
+++ Matriz/Matriz.java
@@ -2,6 +2,8 @@ package Matriz;
 
 public class Matriz {
 
+    private int cant_Filas;
+    private int cant_columnas;
     private ListaPuntos headMatriz;
 
     /**
@@ -21,4 +23,39 @@ public class Matriz {
     public void setHead(ListaPuntos head) {
         this.headMatriz = head;
     }
+
+    public void inicializaMatriz(int c_Filas, int c_columnas){
+
+        ListaPuntos tmp = headMatriz;
+
+        for(int i = 0; i<c_Filas; i++){
+
+        }
+
+
+    }
+
+    public void agregarFilaAlFinal(int numFila) {
+        // Define un nuevo Punto.
+        ListaPuntos nuevaFila = new ListaPuntos(numFila);
+        // Consulta si la lista esta vacia.
+        if (headMatriz == null) {
+            // Inicializa la lista agregando como inicio al nuevo punto.
+            headMatriz = nuevaFila;
+            // Caso contrario recorre la lista hasta llegar al ultimo punto
+            // y agrega el nuevo.
+        } else {
+            // Crea una copia de la lista.
+            ListaPuntos tmp = headMatriz;
+            // Recorre la lista hasta llegar al ultimo punto.
+            while (tmp.getSiguiente() != null) {
+                tmp = tmp.getSiguiente();
+            }
+            // Agrega el nuevo punto al final de la lista.
+            tmp.setSiguiente(nuevaFila);
+        }
+        // Incrementa el contador de tamaño de la lista
+        cant_Filas++;
+    }
+
 }
