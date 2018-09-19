99e414826050b6ec81efb88cec511b2360c1ae2d
kevv87 <7m6JHC84iMsw>
2018-09-19 03:05:10 -0600

    Bases de arduino

--- Conectividad/Client.java
+++ Conectividad/Client.java
@@ -2,13 +2,17 @@ package Conectividad;
 
 import Interfaz.LaminaJuego;
 import Interfaz.MarcoJuego;
+import gnu.io.CommPortIdentifier;
 import java.awt.Color;
+import static java.awt.image.ImageObserver.ERROR;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Enumeration;
 
 
 /**
@@ -25,12 +29,62 @@ public class Client{
 
     private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+    
+    // Variables necesarias para arduino
+    //Variables de conexion
+    private OutputStream output=null;
+    private gnu.io.SerialPort serialPort;
+    private final String PUERTO= "/dev/ttyUSB0";
+    
+    private static final  int TIMEOUT=2000; //Milisegundos
+    
+    private static final int DATA_RATE = 9600;
+    
+    
+    public void inicializarConexion(){
+        CommPortIdentifier puertoID =null;
+        Enumeration puertoEnum = CommPortIdentifier.getPortIdentifiers();
+        
+        while(puertoEnum.hasMoreElements()){  // Mientras existan puertos
+            CommPortIdentifier actualPuertoID = (CommPortIdentifier) puertoEnum.nextElement();
+            if(PUERTO.equals(actualPuertoID.getName())){
+                puertoID=actualPuertoID;
+                break;
+            }
+        }
+        
+        if(puertoID == null){
+            System.exit(ERROR);
+            return;
+        }
+        
+        try{
+            serialPort = (gnu.io.SerialPort) puertoID.open(this.getClass().getName(), TIMEOUT);
+            // Parametros puerto serie
+            
+            serialPort.setSerialPortParams(DATA_RATE, gnu.io.SerialPort.DATABITS_8, gnu.io.SerialPort.STOPBITS_1, gnu.io.SerialPort.PARITY_NONE);
+            output = serialPort.getOutputStream();
+        }catch(Exception e){
+            System.exit(ERROR);
+        }
+    }
+    
+    private void enviarDatosArduino(String datos){
+        try{
+            output.write(datos.getBytes());
+        }catch( Exception e){
+            System.out.println("Hubo un problema con el arduino");
+        }
+    }
     /**
      * Constructor
      */
     public Client() throws Exception{
         pantallaJuego = new MarcoJuego();
+        inicializarConexion();
     }
+    
+    
     
     /**
      * Metodo que maneja toda la logica del cliente, desde crear la conexion hasta manejar el protocolo
@@ -54,8 +108,10 @@ public class Client{
             System.out.println(line.substring(4)+"\n");
           }else if(line.startsWith("YT")){  // Es su turno
             pantallaJuego.setActivo(true);
+            enviarDatosArduino("1");
           }else if(line.startsWith("NYT")){  //No es su turno
               pantallaJuego.setActivo(false);
+              enviarDatosArduino("0");
           }else if(line.startsWith("DWL")){  //Dibuje una linea
               System.out.println(line);
               int punto1 = Integer.parseInt(line.substring(3, 4));
