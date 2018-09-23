3c270befa4ffd2bbe813e09c44588d0d687a52ee
fabian152195 <fabian152195@gmail.com>
2018-09-22 20:51:40 -0600

    saving files

---
+++ Interfaz/LaminaJuego.java
@@ -0,0 +1,139 @@
+package Interfaz;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import javax.swing.JPanel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LaminaJuego extends JPanel{
+    
+    
+   private Punto[] puntos = new Punto[64];
+   private ListaLineas lineas = new ListaLineas();
+   private ListaPoligonos poligonos = new ListaPoligonos();
+   
+   public LaminaJuego(){
+       puntos = new Punto [64]; // Crea un array de circulos
+        int xi = 80;
+        int yi = 60;
+        int radio = 5;
+        int espacio = 50;
+        int cont = 0;
+        
+        // Dibujando puntos
+        for(int i = 0;i<8;i++){
+            for(int j = 0;j<8;j++){
+                puntos[cont] = new Punto(xi,yi, cont);
+                cont+=1;
+                xi+=radio+espacio;
+            }
+            xi = 80;
+            yi += radio+espacio;
+        }
+        
+   }
+   
+    /**
+     * Funcion encargada de dibujar sobre la lamina
+     * @param g Objeto de tipo Graphics
+     */
+    @Override
+    public void paintComponent(Graphics g){
+        System.out.println("Hey");
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        
+        // Dibujando puntos
+        int x;
+        int y;
+        int radio;
+        g2.setPaint(Color.BLACK);
+            for(int j = 0;j<puntos.length;j++){
+                Punto actual = puntos[j];
+                x = actual.getX();
+                y = actual.getY();
+                radio = actual.getRadio();
+                
+                Ellipse2D circulo = new Ellipse2D.Double(); //Se quiere crear un circulo con precision double
+                circulo.setFrameFromCenter(x,y,x+radio,y+radio);  // Coordenadas y dimensiones del circulo      
+                g2.fill(circulo); //Rellena el circulo
+                g2.draw(circulo); //Dibuja el circulo
+            }
+        
+        // Dibujando lineas
+        for(int i = 0;i<lineas.getTamanio();i++){
+            try {
+                Linea linea = lineas.getValor(i);
+                g2.setPaint(linea.getColor());
+                g2.draw(linea);
+            } catch (Exception ex) {
+                Logger.getLogger(LaminaJuego.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }
+        
+        //Dibujando poligonos
+        for(int i = 0;i<poligonos.getTamanio();i++){
+            try {
+                Poligono poligono = poligonos.getValor(i);
+                g2.setPaint(poligono.getColor());
+                g2.fill(poligono);
+            } catch (Exception ex) {
+                Logger.getLogger(LaminaJuego.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+    }
+    
+    /**
+     * Funcion encargada de dibujar lineas entre puntos.
+     * @param id1 Identificacion del primer punto
+     * @param id2 Identificacion del segundo punto
+     * @param color color del que se quiere hacer la linea
+     * @throws java.lang.Exception
+     */
+    public void addLine(int id1, int id2, Color color) throws Exception{
+        
+        Punto punto1 = puntos[id1];
+        Punto punto2 = puntos[id2];
+        int x1 = punto1.getX();
+        int y1 = punto1.getY();
+        int x2 = punto2.getX();
+        int y2 = punto2.getY();
+        
+        if(!lineas.isIn(x1, y1, x2, y2)){
+            lineas.agregarAlInicio(new Linea(x1,y1,x2,y2,color));
+        }
+        repaint();
+    }
+
+    public Punto[] getPuntos() {
+        return puntos;
+    }
+
+    public ListaLineas getLineas() {
+        return lineas;
+    }
+
+    public ListaPoligonos getPoligonos() {
+        return poligonos;
+    }
+    
+    /**
+     * Funcion encargada de .
+     * @param ids Array de ids de los puntos que forman el poligono
+     * @param color Color que se desea que tenga el poligono
+     */
+    public void addPolygon(int [] ids, Color color){
+        Poligono poligono = new Poligono(color);
+        for(int id:ids){
+            Punto punto = puntos[id];
+            poligono.addPoint(punto.getX(), punto.getY());
+        }
+        poligonos.agregarAlInicio(poligono);
+        repaint();
+    }
+}
---
+++ Matriz/Matriz.java
@@ -0,0 +1,232 @@
+package Matriz;
+
+import java.sql.SQLOutput;
+
+public class Matriz {
+
+    private int cant_Filas;
+    private int cant_columnas;
+    private ListaPuntos headMatriz;
+
+    /**
+     * Constructor por defecto
+     */
+    
+    public Matriz(){
+        cant_Filas = 0;
+        cant_columnas = 0;
+        headMatriz = null;
+
+    }
+
+    //GETTER
+    public ListaPuntos getHead() {
+        return headMatriz;
+    }
+
+    public int getCant_Filas() {
+        return cant_Filas;
+    }
+
+
+    public int getCant_columnas() {
+        return cant_columnas;
+    }
+
+    //SETTER
+    public void setHead(ListaPuntos head) {
+        this.headMatriz = head;
+    }
+
+    public void setCant_columnas(int cant_columnas) {
+        this.cant_columnas = cant_columnas;
+    }
+
+    public void setCant_Filas(int cant_Filas) {
+        this.cant_Filas = cant_Filas;
+    }
+
+    ////[FACADE]/////[FUNCIONES PRINCIPALES]//////////////////////////////////////
+
+    /**
+     * Inicializa la matriz con puntos a los que se les asigna integers ordenados de menor a mayor.
+     * @param c_Filas cantidad de filas que desea que tenga la matriz.
+     * @param c_Columnas cantidad de columnas que desea que tenga la matriz.
+     * @return true si se inicializo con exito.
+     */
+
+    public boolean inicializaMatriz(int c_Filas, int c_Columnas){
+
+        //valida que la matriz sea minimo de 2x2
+
+        if (c_Filas >= 2 && c_Columnas > 2){
+
+            this.cant_columnas = c_Columnas; //Modifica atributos de la clase matriz
+            this.cant_Filas = c_Filas;
+            int id = 0;
+
+            agregarFilaAlFinal(id); //primeramente añade una fila, para poder tener acceso a la posicion de memoria del head
+            ListaPuntos tmpH_Matriz = headMatriz;
+
+            //headMatriz = new ListaPuntos(0);
+            //ListaPuntos tmpH = headMatriz;
+
+            for(int i = 1; i <= c_Filas; i++){ //por cada fila
+
+                for(int j=0; j<c_Columnas; j++){ //añade c_Columnas de puntos
+                    tmpH_Matriz.agregarAlFinal(id++,i-1,j);
+
+
+                }
+
+                if (i <= (c_Filas-1)){ // cuando se tiene la cantidad de filas desedas, deje de agregar filas
+                    agregarFilaAlFinal(i);
+                    tmpH_Matriz = tmpH_Matriz.getSiguiente();
+                }
+
+            }
+
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /**
+     * Conecta dos puntos: agrega a la lista de conexiones de ambos puntos la respectiva relacion entre ellos.
+     * @param id1 ID del primer punto.
+     * @param id2 ID del segundo punto.
+     */
+    public void conectarSegmento(int id1, int id2){
+
+        Punto punto1 = buscarPunto(id1);
+        Punto punto2 = buscarPunto(id2);
+
+        boolean conexionValida = validarConexionSegmento(punto1,punto2);
+
+        if (conexionValida){
+
+            punto1.getListaRelaciones().agregarAlInicio(punto2.getId());
+            punto2.getListaRelaciones().agregarAlInicio(punto1.getId());
+        }
+
+    }
+
+    /**
+     * Muestra en consola la matriz.
+     */
+
+    public void printMatriz(){
+
+        ListaPuntos tmp = headMatriz;
+        while(tmp!=null){
+
+            Punto tmp2 = tmp.getHead();
+
+            while (tmp2!=null){
+
+                System.out.print(tmp2.getId() + " ");
+                tmp2 = tmp2.getSiguiente();
+            }
+            System.out.println( " ");
+            tmp = tmp.getSiguiente();
+        }
+    }
+
+    //[FUNCIONES SECUNDARIAS/AUXILIARES]////////////////////////
+
+    /**
+     * Inicializa la matriz con puntos a los que se les asigna integers ordenados de menor a mayor.
+     * @param numFila numero de la fila (según el eje 'y' de la matriz).
+     */
+
+    private void agregarFilaAlFinal(int numFila) {
+        // Define una nueva Fila
+        ListaPuntos nuevaFila = new ListaPuntos(numFila);
+        // Consulta si la matriz esta vacia.
+        if (this.headMatriz == null) {
+            // Inicializa la matriz como inicio de la nueva fila.
+            headMatriz = nuevaFila;
+            // Caso contrario recorre la matriz hasta llegar a la ultima fila.
+            // y agrega el nuevo.
+        } else {
+            // Referencia un puntero temporal al inicio de la matriz.
+            ListaPuntos tmp = headMatriz;
+            // Recorre la matriz hasta llegar a la ultima fila.
+            while (tmp.getSiguiente() != null) {
+                tmp = tmp.getSiguiente();
+            }
+            // Agrega la nueva fila al final de la matriz.
+            tmp.setSiguiente(nuevaFila);
+        }
+        // Incrementa el contador de cantidad de filas de la matriz.
+        this.cant_Filas++;
+    }
+
+
+    /**
+     * Busca un punto en la matriz, dado el ID.
+     * @param id ID del punto deseado.
+     * @return Punto buscado.
+     */
+
+    private Punto buscarPunto(int id){
+
+        ListaPuntos tmp = headMatriz;
+
+        while (tmp != null){
+
+            Punto tmp2 = tmp.getHead();
+
+            while (tmp2!= null){
+                if(tmp2.getId() == id){
+                    return tmp2;
+                }
+                tmp2 = tmp2.getSiguiente();
+            }
+
+            tmp = tmp.getSiguiente();
+        }
+        return null;
+    }
+
+    /**
+     * Valida el tamano del segmento entre dos puntos.
+     * @param punto1 punto de referencia.
+     * @param punto2 punto anexo.
+     * @return true si la longitud es menor o igual a 1 tanto en 'X' como 'Y'.
+     */
+    private boolean validarLongitudSegmento(Punto punto1, Punto punto2){
+
+        int distSegX = Math.abs(punto1.getN_columna() - punto2.getN_columna()); //distancia en eje X del segmento
+        int distSegY = Math.abs(punto1.getN_fila() - punto2.getN_fila()); // distancia en eje Y del segmento
+        
+        return distSegX <= 1 && distSegY <= 1;
+    }
+
+
+    /**
+     * Valida si el segmento no existe, y si no se encuentra dentro de un area
+     * @param punto1 punto 1
+     * @param punto2 punto 2
+     * @return true si la conexion es posible
+     */
+    private boolean validarConexionSegmento(Punto punto1, Punto punto2){ // si el segmento ya existe o si se encuentra dentro de un area.
+
+        //primero valida longitud del segmento
+        boolean bool_longitud = validarLongitudSegmento(punto1, punto2);
+
+        if (bool_longitud){
+
+            boolean existeConexion = punto1.getListaRelaciones().buscar(punto2.getId());
+            return !existeConexion;
+            
+        }
+        return false;
+    }
+
+
+
+
+}
